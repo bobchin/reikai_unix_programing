630ae5038e766506ca4c788b627aaa0cf3525463
bobchin <bobchin.ryu@gmail.com>
2015-10-05 14:22:59 +0900

    prac1-10

---
+++ chapter01/prac1-10.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+struct point {
+  int x;
+  int y;
+};
+
+int point_is_equal(struct point *, struct point *);
+
+int main(void)
+{
+  struct point z1 = { 100, 200 };
+  struct point z2 = { 100, 200 };
+  struct point z3 = { 101, 200 };
+  struct point z4 = { 100, 201 };
+  struct point z5 = { 101, 201 };
+
+  printf("z1==z2:%d\n", point_is_equal(&z1, &z2));
+  printf("z1==z3:%d\n", point_is_equal(&z1, &z3));
+  printf("z1==z4:%d\n", point_is_equal(&z1, &z4));
+  printf("z1==z5:%d\n", point_is_equal(&z1, &z5));
+
+  return 0;
+}
+
+int point_is_equal(struct point *p1, struct point *p2)
+{
+  return (p1->x == p2->x && p1->y == p2->y);
+}
