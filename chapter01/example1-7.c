09452135eca9876b0960d14d34b01f9d8f657cd6
bobchin <bobchin.ryu@gmail.com>
2015-10-05 15:43:14 +0900

    example1-7

---
+++ chapter01/example1-7.c
@@ -0,0 +1,17 @@
+union foo {int i; double d;};
+
+enum Type {Int, Double};
+
+struct bar {
+  enum Type type;
+  union foo u;
+};
+
+int main(void)
+{
+  struct bar x;
+  x.u.d = 12.34;
+  x.type = Double;
+
+  return 0;
+}
