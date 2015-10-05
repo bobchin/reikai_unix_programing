b4b516a0dbd453e5ecf4e5195a21b7a567100820
bobchin <bobchin.ryu@gmail.com>
2015-10-05 17:33:13 +0900

    prac2-3

---
+++ chapter02/prac2-3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+char *itoa(int i)
+{
+  char buf[1024];
+  snprintf(buf, sizeof(buf), "%d", i);
+  return buf;
+}
+
+int main(void)
+{
+  char *p = itoa(1234);
+  printf("p=%s\n", p);
+  return 0;
+}
