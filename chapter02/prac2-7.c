0c08d84fe6c61dcf750ec8d6b557c59fda0f07d3
bobchin <bobchin.ryu@gmail.com>
2015-10-06 09:29:39 +0900

    prac2-7

---
+++ chapter02/prac2-7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct point {int x, y;};
+
+int main(void)
+{
+  struct point *p = malloc(sizeof(struct point));
+
+  int *q = &p->x;
+  p->x = 10;
+  p->y = 20;
+  free(p);
+  printf("%d\n", *q);
+
+  return 0;
+}
